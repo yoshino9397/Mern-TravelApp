350566af2a43019b6975a3c0a10c3c90afebc112
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-24 17:57:18 -0700

    fix login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -30,14 +30,17 @@ router.post("/login", async (req, res) => {
   try {
     //find user
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong username or password");
-
+    if (!user) {
+      return res.status(404).json("Wrong username or password");
+    }
     //validate password
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
-    !validPassword && res.status(400).json("Wrong username or password");
+    if (!validPassword) {
+      return res.status(400).json("Wrong username or password");
+    }
 
     //send response
     res.status(200).json({ _id: user._id, username: user.username });
